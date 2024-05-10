13d61920e146208470f4123de388e95b9f88034f
y1z <whyis2cool@gmail.com>
2024-05-09 22:34:01 -0600

    Add spheres and scripts

--- Assets/LMAO.cs
+++ Assets/LMAO.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class LMAO : MonoBehaviour
 {
-
-    public float ftdsu;
-
-    public LMAO Ld;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Holiwis");
+        
     }
 
     // Update is called once per frame
